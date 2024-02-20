ade005cbd109414966bc3532d2d33a90f46ee8d3
vvv9912 <vvv9912@github.com>
2024-02-20 21:14:20 +0300

    add migrations

---
+++ migrations/migrations.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"embed"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
+	"log"
+	"os"
+)
+
+//go:embed migrations/*
+var migrations embed.FS
+
+func Migrate(db *sqlx.DB) error {
+	goose.SetBaseFS(migrations)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("postgres migrate set dialect postgres: %w", err)
+	}
+	dir, err := os.Getwd()
+	log.Println(dir)
+	_ = err
+	if err := goose.Up(db.DB, "migrations"); err != nil {
+
+		return fmt.Errorf("postgres migrate up: %w", err)
+	}
+	if err := db.Close(); err != nil {
+		return fmt.Errorf("postgres migrate close: %w", err)
+	}
+
+	return nil
+}
