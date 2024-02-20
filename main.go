d18a0916831a30ecc332c96407d08fff15844bc1
vvv9912 <vvv9912@github.com>
2024-02-20 21:14:10 +0300

    add migrations

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "log"
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/vvv9912/sddb/migrations"
+	"log"
+)
 
 func main() {
 	log.Println("Hello, World!")
-
+	database_dsn := "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	db, err := sqlx.Connect("postgres", database_dsn)
+	if err != nil {
+		log.Println("error connect config.Get().DatabaseDSN\n ", database_dsn, "db err:", db)
+		return
+	}
+	if err := migrations.Migrate(db); err != nil {
+		log.Fatal(err)
+	}
 }
